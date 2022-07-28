79aab130b1d7a2ba3020aa07009028b755a2bb53
Daniel Coady <me@pondo.dev>
2022-07-28 20:19:33 +1000

    add bacapad keymap

---
+++ keyboards/dumbpad/customkbd/keymaps/bacapad-vial/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// TODO: probably have a unique id for this idk
+#define VIAL_KEYBOARD_UID {0x90, 0xD4, 0xF9, 0xA4, 0xDD, 0x1E, 0x74, 0xF6}
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_MUSIC_MODE
+#define LAYER_STATE_8BIT
